a1b52cfcf127b00fd0656fbf904b0b4a793a6adc
taptap <alexey.e.egorov@gmail.com>
2014-05-28 17:09:17 +0400

    fix stupid repository mess

--- statistics/statisticsapi.h
+++
@@ -1,6 +0,0 @@
-#ifndef STATISTICSAPI_H
-#define STATISTICSAPI_H
-
-enum StatTypes { StyleSuccess, StylesUnsuccess, NumberSuccess, NumberUnsuccess, /*ColorsSuccess, ColorsUnsuccess,*/ Total, StatTypesCount };
-
-#endif // STATISTICSAPI_H
